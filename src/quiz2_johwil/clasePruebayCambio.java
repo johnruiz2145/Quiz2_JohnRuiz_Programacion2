0e86adfa0065352f57fc13611cc435a1bcc75db8
SALA-4 <SALA-4@SI-04-DOC.salas.local>
2025-10-10 09:43:31 -0500

    Cree clase nueva para prgrama con manuel

---
+++ src/quiz2_johwil/clasePruebayCambio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quiz2_johwil;
+
+/**
+ *
+ * @author SALA-4
+ */
+public class clasePruebayCambio {
+    
+}
